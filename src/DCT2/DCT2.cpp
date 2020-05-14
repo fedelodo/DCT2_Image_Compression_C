76f6f20db5b566a8fe595409400262b1763fdb94
Jan <emagiannu@yahoo.it>
2020-05-14 14:13:30 +0200

    Cleanup NO AUTO

--- src/DCT2/DCT2.cpp
+++ src/DCT2/DCT2.cpp
@@ -1,14 +1,15 @@
 
+#define _USE_MATH_DEFINES
+
 #include "DCT2.h"
 #include <math.h>
-#define _USE_MATH_DEFINES
 ///This function computes DCT2 of a matrix of int
 DCT2::DCT2matrix *DCT2::DCT2Compute(const int *matrix) {
     // allocando la memoria come  type *array = new type[sizeX*sizeY];
     // consente di gestire in modo più efficente gli array in quanto alloca un unico
     // blocco di memoria l' accesso all' elemento array[i][j] può essere fatto con array[i*sizeY+j]
     const  int size = sizeof(matrix)/sizeof(matrix[0]);
-    auto *c = new float[size*size];
+    DCT2matrix *c = new DCT2matrix[size * size];
     DCT2::DCT2matrix sum;
     int l,k,i,j,N;
     N=size;
@@ -20,8 +21,8 @@ DCT2::DCT2matrix *DCT2::DCT2Compute(const int *matrix) {
             {
                 for (j = 0; j < N; j++) {
                     sum += matrix[i*size+j]*
-                            cos(k*M_PI*((2.0f*(DCT2::DCT2matrix)i+1)/2.0f*(DCT2::DCT2matrix)N))*
-                            cos(l*M_PI*((2.0f*(DCT2::DCT2matrix)j+1)/(2.0f*(DCT2::DCT2matrix)N)));
+                            cos(k*M_PI*((2*(DCT2::DCT2matrix)i+1)/2*(DCT2::DCT2matrix)N))*
+                            cos(l*M_PI*((2*(DCT2::DCT2matrix)j+1)/(2*(DCT2::DCT2matrix)N)));
                 }
             }
             c[i*size+j] = alphakl(k,l,N) * sum;
@@ -31,14 +32,14 @@ DCT2::DCT2matrix *DCT2::DCT2Compute(const int *matrix) {
 }
 
 ///This functions computes alphakl values
-float DCT2::alphakl(int k, int l, int N) {
+DCT2::DCT2matrix DCT2::alphakl(int k, int l, int N) {
     if(k > 0 && l > 0) {
-        return 2.0f/(float)N;
+        return 2/(DCT2matrix)N;
     }
     else if(l == 0 || k == 0) {
-        return M_SQRT2/(float)N;
+        return M_SQRT2/(DCT2matrix)N;
     }
     else {
-        return 1.0f/(float)N;
+        return 1/(DCT2matrix)N;
     }
 }
