f8842a835e94cd17718ba38161cc33a979c63aab
Jan <emagiannu@yahoo.it>
2020-05-14 15:20:24 +0200

    Fixed ( in DTC2 and print

--- src/DCT2/DCT2.cpp
+++ src/DCT2/DCT2.cpp
@@ -3,27 +3,29 @@
 
 #include "DCT2.h"
 #include <math.h>
+#include <iostream>
+
 ///This function computes DCT2 of a matrix of int
 DCT2::DCT2matrix *DCT2::DCT2Compute2D(const int *matrix, int size) {
     // allocando la memoria come  type *array = new type[sizeX*sizeY];
     // consente di gestire in modo più efficente gli array in quanto alloca un unico
     // blocco di memoria l' accesso all' elemento array[i][j] può essere fatto con array[i*sizeY+j]
-    auto *c = new DCT2::DCT2matrix[size*size];
+    DCT2matrix *c;
+    c = new DCT2::DCT2matrix[size * size];
     DCT2::DCT2matrix sum;
     int l,k,i,j,N;
     N=size;
-    for(k=0; k < N; k++)
-    {
+    for(k=0; k < N; k++) {
         for (l = 0; l < N; l++) {
             sum = 0;
-            for (i = 0; i < N; i++)
-            {
+            for (i = 0; i < N; i++) {
                 for (j = 0; j < N; j++) {
                     sum += matrix[i*size+j]*
-                            cos(k*M_PI*((2.0f*(float)i+1)/2.0f*(float)N))*
-                            cos(l*M_PI*((2.0f*(float)j+1)/(2.0f*(float)N)));
+                            cos(k*M_PI*((2.0f*(float)i+1.0f)/(2.0f*(float)N))) *
+                            cos(l*M_PI*((2.0f*(float)j+1.0f)/(2.0f*(float)N)));
                 }
             }
+            std::cout << sum << std::endl;
             c[k*size+l] = alpha2D(k,l,N) * sum;
         }
     }
@@ -44,6 +46,7 @@ DCT2::DCT2matrix *DCT2::DCT2Compute1D(const int *array, int size) {
             {
                     sum += array[i]*
                            cos(k*M_PI*((2.0f*(float)i+1)/2.0f*(float)N));
+
                 }
             c[k] = alpha1D(k,N) * sum;
         }
@@ -52,14 +55,14 @@ DCT2::DCT2matrix *DCT2::DCT2Compute1D(const int *array, int size) {
 
 ///This functions computes alphakl values
 float DCT2::alpha2D(int k, int l, int N) {
-    if(k > 0 && l > 0) {
-        return 2.0f/(float)N;
+    if(k == 0 && l == 0) {
+        return 1.0f/(float)N;
     }
     else if(l == 0 || k == 0) {
         return M_SQRT2/(float)N;
     }
     else {
-        return 1.0f/(float)N;
+        return 2.0f/(float)N;
     }
 }
 
