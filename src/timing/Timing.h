4c61db97e587a4eadcaea4927d6b9b7a88b2814d
Jan <emagiannu@yahoo.it>
2020-05-18 16:04:56 +0200

    Removed resolution

--- src/timing/Timing.h
+++ src/timing/Timing.h
@@ -14,7 +14,7 @@ public:
     std::vector<int> getResults();
     void generateCSV(std::string filename);
 
-    static inputsType generateInputs(int samples, int resolution);
+    static inputsType generateInputs(int samples);
 
 private:
     std::vector<int> _results;
